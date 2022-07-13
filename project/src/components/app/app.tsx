e176d39cc0ed47aa4cf3367528d93148bf7c2f27
Nikita Tolstik <tolstik-nik@mail.ru>
2022-07-14 02:16:53 +0300

    Подключает и настраивает react-router-dom в App

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import AuthScreen from '../../pages/auth-screen/auth-screen';
+
 
 type AppScreenProps = {
   cardsCount: number;
@@ -6,7 +12,26 @@ type AppScreenProps = {
 
 function App({ cardsCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen cardsCount={cardsCount} />}
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<PropertyScreen />}
+        />
+        <Route
+          path={AppRoute.Favorite}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Auth}
+          element={<AuthScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
