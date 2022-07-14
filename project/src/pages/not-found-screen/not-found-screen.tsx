f66feedc7172992130605d9d988c4852104e7dc6
Nikita Tolstik <tolstik-nik@mail.ru>
2022-07-14 16:49:33 +0300

    Добавляет Logo в компоненты страниц

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFoundScreen(): JSX.Element {
   return (
@@ -7,9 +8,7 @@ function NotFoundScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
           </div>
         </div>
@@ -20,7 +19,7 @@ function NotFoundScreen(): JSX.Element {
           <section>
             <h1 className="not__found__error">404</h1>
             <p className="not__found__error__text">PAGE NOT FOUND</p>
-            <Link className="not__found__link__home__page" to='/'>HOME PAGE</Link>
+            <Link className="not__found__link__home__page" to="/">HOME PAGE</Link>
           </section>
         </div>
       </main >
