5dee34fdf79741818c9a02187a367c244cf9a9cf
Nikita Tolstik <tolstik-nik@mail.ru>
2022-07-28 17:29:19 +0300

    6.4 Подключает store в App

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { offers, favoriteOffers, nearPlacesOffers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,11 +13,13 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      favoriteOffers={favoriteOffers}
-      nearPlacesOffers={nearPlacesOffers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        offers={offers}
+        favoriteOffers={favoriteOffers}
+        nearPlacesOffers={nearPlacesOffers}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
 );
