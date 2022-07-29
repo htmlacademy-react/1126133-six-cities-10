b608adf5c5f27c2b438bb9fffa59c99e96095031
Nikita Tolstik <tolstik-nik@mail.ru>
2022-07-29 22:04:44 +0300

    6.7 Заполянет список предложений в state в компоненте App. Убирает передачу главного списка через пропсы в index.tsx, app.tsx

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { offers, favoriteOffers, nearPlacesOffers } from './mocks/offers';
+import { favoriteOffers, nearPlacesOffers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
 import { store } from './store';
 
@@ -10,12 +10,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App
-        offers={offers}
         favoriteOffers={favoriteOffers}
         nearPlacesOffers={nearPlacesOffers}
         reviews={reviews}
