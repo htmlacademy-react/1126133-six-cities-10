1c2df017a35c9c9218ff0f36bf7d513f1b326898
Nikita Tolstik <tolstik-nik@mail.ru>
2022-07-30 04:26:48 +0300

    6.16 Подключает компоненты main-offers и main-offers-empty в MainScreen

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,10 +1,10 @@
 import { PageCardClass } from '../../const';
 import Logo from '../../components/logo/logo';
-import OffersList from '../../components/offers-list/offers-list';
-import Map from '../../components/map/map';
 import CitiesListTabs from '../../components/cities-list-tabs/cities-list-tabs';
 import { useAppSelector } from '../../hooks';
 import { getActiveCityOffers } from '../../utils/utils';
+import MainOffers from '../../components/main-offers/main-offers';
+import MainOffersEmpty from '../../components/main-offers-empty/main-offers-empty';
 
 function MainScreen(): JSX.Element {
   const { offers, activeCity } = useAppSelector((state) => state);
@@ -12,6 +12,8 @@ function MainScreen(): JSX.Element {
   const activeCityOffers = getActiveCityOffers(activeCity, offers);
 
   const offersCount = activeCityOffers.length;
+
+  const isEmptyOffers = !offersCount;
 
   return (
     <div className="page page--gray page--main">
@@ -42,7 +44,9 @@ function MainScreen(): JSX.Element {
         </div>
       </header>
 
-      <main className="page__main page__main--index">
+      <main
+        className={`page__main page__main--index ${isEmptyOffers ? 'page__main--index-empty' : ''}`}
+      >
         <h1 className="visually-hidden">Cities</h1>
 
         <div className="tabs">
@@ -54,46 +58,12 @@ function MainScreen(): JSX.Element {
         </div>
 
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
 
-              <b className="places__found">
-                {offersCount} places to stay in Amsterdam
-              </b>
+          {/* Такая запись условного рендеринга подходит в этой ситуации ? Или лучше делать другой вариант из-за плохой читаемости кода ? */}
+          {isEmptyOffers
+            ? <MainOffersEmpty activeCity={activeCity} />
+            : <MainOffers offersCount={offersCount} activeCityOffers={activeCityOffers} cardClass={PageCardClass.Main} activeCity={activeCity} />}
 
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
-
-              <div className="cities__places-list places__list tabs__content">
-                <OffersList
-                  offers={activeCityOffers}
-                  cardClass={PageCardClass.Main}
-                />
-              </div>
-            </section>
-
-            <div className="cities__right-section">
-              <Map
-                activeCity={activeCity}
-                offers={activeCityOffers}
-              />
-            </div>
-
-          </div>
         </div>
       </main>
     </div>
